6a1d28131cbb6b5d201d73bf428d15d41c724d7e
arunsnd2017 <arunsinghsnd@gmail.com>
2020-06-20 19:07:59 +0530

    Updating our styles to use media queries for mobile for menu items and collections perview

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route , Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector} from 'reselect';
 
-import './App.css';
+import { GlobalStyle } from './global.styles';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -49,6 +49,7 @@ componentWillUnmount() {
   render(){
     return (
       <div>
+      <GlobalStyle/>
       <Header/>
        <Switch>
        <Route exact path='/' component={HomePage}  />
